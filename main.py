69e550ea879bd31235b7402bf2172e6d739b6308
unknown <petrucretu03@gmail.com>
2024-08-14 14:12:36 +0300

    modificare

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from ChiulangiiSiSilitori import *
 def main():
     db_config = {
@@ -31,24 +31,5 @@ if __name__ == "__main__":
     main()
 
 
-
-=======
-from functie4 import *
-
-if __name__=="__main__":
-
-    db = BazaDeDate(host='localhost', password='root', user='root', database='virtuals') #din virtuals luam majoritatea datelor 
-    procesareInfoBazadate=InitiereBazaDate(db)
-    generareRapoarte=generatorDeRapoarte(folder_backup='backup')
-    generatorNotificari = trimitereNotificari(db)
-
-
-    today=datetime.now.strftime('%Y-%m-%d')
-    ore_lucrate=procesareInfoBazadate.calculate_ore_lucrate(today)
-    angajatiNotificati=procesareInfoBazadate.get_angajatiNotificati(today)
-    if angajatiNotificati:
-        generatorDeRapoarte.generareRapoarte(angajatiNotificati)
-        trimitereNotificari.trimiteMail(angajatiNotificati)
->>>>>>> functie1
 
 
